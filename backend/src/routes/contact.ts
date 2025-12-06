365bbcb625fdbbd33cf178f8a48af87ee455556c
emciiowhy <mcmcyap07@gmail.com>
2025-12-06 16:39:07 +0800

    fix gmail

--- backend/src/routes/contact.ts
+++ backend/src/routes/contact.ts
@@ -18,6 +18,7 @@ const transporter = nodemailer.createTransport({
     pass: process.env.GMAIL_APP_PASSWORD || 'kncoorrokjochxuc',
   },
   connectionTimeout: 10000,
+  greetingTimeout: 10000,
   socketTimeout: 10000,
   tls: {
     rejectUnauthorized: false
